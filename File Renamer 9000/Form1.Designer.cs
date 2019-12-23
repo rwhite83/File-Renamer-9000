b5d3a053b178d10a8a002423aa67692fbb61e834
rwhite83 <rwhite83@hotmail.com>
2019-12-22 17:40:44 -0800

    version 1.0 complete

---
+++ File Renamer 9000/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace File_Renamer_9000
+{
+    partial class fileRenamer9000
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderPickerTextBox = new System.Windows.Forms.TextBox();
+            this.selectFolderButton = new System.Windows.Forms.Button();
+            this.startLabel = new System.Windows.Forms.Label();
+            this.startNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.namingFormatTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.instructiosLlabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.startNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // folderPickerTextBox
+            // 
+            this.folderPickerTextBox.Location = new System.Drawing.Point(30, 52);
+            this.folderPickerTextBox.Name = "folderPickerTextBox";
+            this.folderPickerTextBox.Size = new System.Drawing.Size(800, 31);
+            this.folderPickerTextBox.TabIndex = 0;
+            // 
+            // selectFolderButton
+            // 
+            this.selectFolderButton.Location = new System.Drawing.Point(947, 38);
+            this.selectFolderButton.Name = "selectFolderButton";
+            this.selectFolderButton.Size = new System.Drawing.Size(171, 59);
+            this.selectFolderButton.TabIndex = 3;
+            this.selectFolderButton.Text = "Select Folder";
+            this.selectFolderButton.UseVisualStyleBackColor = true;
+            this.selectFolderButton.Click += new System.EventHandler(this.SelectFolder_Click);
+            // 
+            // startLabel
+            // 
+            this.startLabel.AutoSize = true;
+            this.startLabel.Location = new System.Drawing.Point(85, 304);
+            this.startLabel.Name = "startLabel";
+            this.startLabel.Size = new System.Drawing.Size(138, 25);
+            this.startLabel.TabIndex = 4;
+            this.startLabel.Text = "Start Number";
+            // 
+            // startNumericUpDown
+            // 
+            this.startNumericUpDown.Location = new System.Drawing.Point(306, 304);
+            this.startNumericUpDown.Maximum = new decimal(new int[] {
+            1661992959,
+            1808227885,
+            5,
+            0});
+            this.startNumericUpDown.Name = "startNumericUpDown";
+            this.startNumericUpDown.Size = new System.Drawing.Size(200, 31);
+            this.startNumericUpDown.TabIndex = 6;
+            // 
+            // namingFormatTextBox
+            // 
+            this.namingFormatTextBox.Location = new System.Drawing.Point(30, 234);
+            this.namingFormatTextBox.Name = "namingFormatTextBox";
+            this.namingFormatTextBox.Size = new System.Drawing.Size(800, 31);
+            this.namingFormatTextBox.TabIndex = 8;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(947, 220);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(171, 59);
+            this.startButton.TabIndex = 9;
+            this.startButton.Text = "Start Rename";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // instructiosLlabel
+            // 
+            this.instructiosLlabel.Location = new System.Drawing.Point(78, 136);
+            this.instructiosLlabel.Name = "instructiosLlabel";
+            this.instructiosLlabel.Size = new System.Drawing.Size(508, 63);
+            this.instructiosLlabel.TabIndex = 10;
+            this.instructiosLlabel.Text = "Write your desired rename format here.  Use # for where you\'d like numbers, but o" +
+    "nly in one place";
+            // 
+            // fileRenamer9000
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1180, 390);
+            this.Controls.Add(this.instructiosLlabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.namingFormatTextBox);
+            this.Controls.Add(this.startNumericUpDown);
+            this.Controls.Add(this.startLabel);
+            this.Controls.Add(this.selectFolderButton);
+            this.Controls.Add(this.folderPickerTextBox);
+            this.Name = "fileRenamer9000";
+            this.Text = "Form Renamer 9000";
+            ((System.ComponentModel.ISupportInitialize)(this.startNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox folderPickerTextBox;
+        private System.Windows.Forms.Button selectFolderButton;
+        private System.Windows.Forms.Label startLabel;
+        private System.Windows.Forms.NumericUpDown startNumericUpDown;
+        private System.Windows.Forms.TextBox namingFormatTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label instructiosLlabel;
+    }
+}
+
---
+++ File Renamer 9000/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Renamer_9000
+{
+    public partial class fileRenamer9000 : Form
+    {
+
+        int numStart;
+        int numEnd;
+        int numCurrent;
+        int numOfDigits;
+        int numOfHash;
+        string newFileNameFormat;
+
+        public fileRenamer9000()
+        {
+            InitializeComponent();
+            numOfHash = 0;
+        }
+
+        private void SelectFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog rootPicker = new FolderBrowserDialog();
+            DialogResult result = rootPicker.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(rootPicker.SelectedPath))
+            {
+                folderPickerTextBox.Text = rootPicker.SelectedPath;
+                newFileNameFormat = namingFormatTextBox.Text;
+            }
+        }
+
+        private string nextTitle(string originalName, string originalPath, string originalExtension)
+        {
+            bool numProcessed = false;
+            StringBuilder sb = new StringBuilder(originalPath);
+            sb.Append('\\');
+            for (int i = 0; i < newFileNameFormat.Length;)
+            {
+                if (newFileNameFormat[i] == '#')
+                {
+                    if (!numProcessed)
+                    {
+                        string formatString = String.Format("{0:D" + numOfHash + "}", numCurrent);
+                        sb.Append(formatString);
+                        i += numOfDigits;
+                        numProcessed = true;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+                else
+                {
+                    sb.Append(newFileNameFormat[i]);
+                    i++;
+                }
+            }
+            sb.Append(originalExtension);
+            string returnString = sb.ToString();
+            return returnString;
+        }
+
+        private void getHashCount()
+        {
+            numOfHash = 0;
+            foreach (char c in newFileNameFormat)
+            {
+                if (c == '#')
+                {
+                    numOfHash++;
+                }
+            }
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            numCurrent = (int)startNumericUpDown.Value;
+            newFileNameFormat = namingFormatTextBox.Text;
+            numStart = (int)startNumericUpDown.Value;
+            getHashCount();
+            if (numOfHash == 0)
+            {
+                MessageBox.Show("You must have at least one hashtag within your rename format please");
+            }
+            else
+            {
+                try
+                {
+                    string targetDirectoryString = folderPickerTextBox.Text;
+                    DirectoryInfo targetDirectoryInfo = new DirectoryInfo(targetDirectoryString);
+                    int numberOfNums = targetDirectoryString.Length;
+                    Console.WriteLine("numStart " + numStart + " numberOfNums: " + numberOfNums);
+                    numOfDigits = (int)Math.Floor(Math.Log10(numStart + numberOfNums) + 1);
+                    Console.WriteLine("dig: " + numOfDigits + " hash: " + numOfHash);
+                    numOfHash = numOfDigits > numOfHash ? numOfDigits : numOfHash;
+                    Console.WriteLine("dig: " + numOfDigits + " hash: " + numOfHash);
+                    if (numOfDigits > numOfHash)
+                    {
+                        numOfHash = numOfDigits;
+                    }
+                    foreach (FileInfo file in targetDirectoryInfo.GetFiles())
+                    {
+                        string newFileName = nextTitle(file.Name, file.DirectoryName, file.Extension);
+                        numCurrent++;
+                        Console.WriteLine(newFileName);
+                        file.MoveTo(newFileName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (ex is DirectoryNotFoundException || ex is System.ArgumentException)
+                    {
+                        MessageBox.Show("You must select a valid filepath please");
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
